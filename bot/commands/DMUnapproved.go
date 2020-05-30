a609bbc4b5460cc54123d442962a90231723f846
Malborne <ia1306439@qu.edu.qa>
2020-05-30 14:54:34 +0300

    dmunapproved

--- bot/commands/DMUnapproved.go
+++ bot/commands/DMUnapproved.go
@@ -39,21 +39,19 @@ func commandDMUnapproved(s *discordgo.Session, m *discordgo.MessageCreate, args 
 				"You are an unapproved member of Learn/Memorize Quran Server and you are about to lose access to most of the server. If you still wish to retain access to the server, please contact one of the moderators in the #approval-and-verification channel below to be approved.\n  https://discord.gg/tY2eMR \n\nYou cannot reply to this message."))
 			if err != nil {
 				return errors.Wrap(err, "sending message failed")
+			} else {
+				count = count + 1
 			}
-			// if err == nil {
-			// 	count = count + 1
-			// }
 		}
 	}
 
 	if count == 0 {
 		_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("No unapproved users found."))
 		return errors.Wrap(err, "sending message failed")
+	} else {
+		_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent messaged to %s users", count))
+		return errors.Wrap(err, "sending message failed")
 	}
-	// else {
-	// 	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent messaged to %s users",count))
-	// 	return errors.Wrap(err, "sending message failed")
-	// }
 
 	err = s.MessageReactionAdd(m.ChannelID, m.ID, "✅")
 	return errors.Wrap(err, "adding reaction failed")
