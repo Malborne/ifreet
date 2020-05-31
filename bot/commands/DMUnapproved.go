90a99e47f344db79a6829b6c0d95fc933055c969
Malborne <ia1306439@qu.edu.qa>
2020-05-31 09:19:38 +0300

    approval message and dmunapproved

--- bot/commands/DMUnapproved.go
+++ bot/commands/DMUnapproved.go
@@ -30,13 +30,13 @@ func commandDMUnapproved(s *discordgo.Session, m *discordgo.MessageCreate, args 
 	var count int = 0
 	for _, member := range guild.Members {
 
-		if !isApproved(member) && member.User.ID != s.State.User.ID {
+		if !isApproved(member) && member.User.ID != s.State.User.ID && member.User.ID != 680170745627934742 && member.User.ID != 714905000668692541 { //IDs for Admin and supermod
 			userChannel, err := s.UserChannelCreate(member.User.ID)
 			if err != nil {
 				return errors.Wrap(err, "creating private channel failed")
 			}
 			_, err = s.ChannelMessageSend(userChannel.ID, fmt.Sprintf(
-				"You are an unapproved member of Learn/Memorize Quran Server and you are about to lose access to most of the server. If you still wish to retain access to the server, please contact one of the moderators in the #approval-and-verification channel below to be approved.\n  https://discord.gg/tY2eMR \n\nYou cannot reply to this message."))
+				"You are an unapproved member of Learn/Memorize Quran Server and you are about to lose access to most of the server. If you still wish to retain access to the server, please contact one of the moderators in the #approval-and-verification channel below to be approved.\n  https://discord.gg/R6jKWT \n\nYou cannot reply to this message."))
 			if err != nil {
 				return errors.Wrap(err, "sending message failed")
 			} else {
