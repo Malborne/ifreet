03af965544246949cce671d9997bb2ad2fa88650
Malborne <ia1306439@qu.edu.qa>
2020-06-06 11:21:42 +0300

    cannot warn higher roles

--- bot/commands/warn.go
+++ bot/commands/warn.go
@@ -59,7 +59,7 @@ func commandWarnUser(s *discordgo.Session, m *discordgo.MessageCreate, args doco
 		return errors.Wrap(err, "sending message failed")
 	}
 
-	if isOneLowerThanTwo(*infractor, *m.Member) {
+	if isOneLowerThanTwo(infractor, m.Member) {
 		_, err := s.ChannelMessageSend(m.ChannelID, "You cannot warn a user that has the same or a role higher than you")
 		return errors.Wrap(err, "sending message failed")
 	}
