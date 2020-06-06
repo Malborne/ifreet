5563423ce092f2acc08a043f9dc83f28238a5e4e
Malborne <ia1306439@qu.edu.qa>
2020-06-06 14:05:18 +0300

    cannot warn higher roles

--- bot/commands/warn.go
+++ bot/commands/warn.go
@@ -73,6 +73,7 @@ func commandWarnUser(s *discordgo.Session, m *discordgo.MessageCreate, args doco
 	}
 
 	if isOneLowerThanTwo(author, infractor) {
+		_, _ = s.ChannelMessageSend(heimdallr.Config.AdminLogChannel, fmt.Sprintf("%s the infractor has rank of: %s and %s the author has rank of: %s", infractor.Mention(), getHighestRole(infractor), author.Mention(), getHighestRole(author)))
 		_, err := s.ChannelMessageSend(m.ChannelID, "You cannot warn a user that has the same or a role higher than you")
 		return errors.Wrap(err, "sending message failed")
 	}
@@ -121,9 +122,6 @@ func commandWarnUser(s *discordgo.Session, m *discordgo.MessageCreate, args doco
 		return nil
 		// return errors.Wrap(err, "sending message failed")
 	}
-
-	_, err = s.ChannelMessageSend(heimdallr.Config.AdminLogChannel, fmt.Sprintf("%s the infractor has rank of: %s and %s the author has rank of: %s", infractor.Mention(), getHighestRole(infractor), author.Mention(), getHighestRole(author)))
-	return errors.Wrap(err, "sending message failed")
 
 	return errors.Wrap(err, "adding reaction failed")
 
