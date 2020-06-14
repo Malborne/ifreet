29740b7887b9c0ad58083917b5573211b993029f
Malborne <ia1306439@qu.edu.qa>
2020-06-14 18:42:05 +0300

    clear

--- bot/commands/clear.go
+++ bot/commands/clear.go
@@ -24,7 +24,7 @@ var clearCommand = command{
 
 //commandWarnUser warns another user and gives an infraction.
 func commandClearMessages(s *discordgo.Session, m *discordgo.MessageCreate, args docopt.Opts) error {
-	number, _ := args.Int("<number>")
+	// number, _ := args.Int("<number>")
 
 	guildID := m.GuildID
 
@@ -39,24 +39,22 @@ func commandClearMessages(s *discordgo.Session, m *discordgo.MessageCreate, args
 		return errors.Wrap(err, "sending message failed")
 	}
 
-	prompt, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Are you sure you want to clear %s messages? This cannot be undone.", number))
-	return errors.Wrap(err, "sending message failed")
-	err = s.MessageReactionAdd(m.ChannelID, prompt.ID, "✅")
-	return errors.Wrap(err, "adding reaction failed")
-	err = s.MessageReactionAdd(m.ChannelID, prompt.ID, "❌")
-	return errors.Wrap(err, "adding reaction failed")
+	// prompt, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Are you sure you want to clear %s messages? This cannot be undone.", number))
+	// return errors.Wrap(err, "sending message failed")
+	// err = s.MessageReactionAdd(m.ChannelID, prompt.ID, "✅")
+	// return errors.Wrap(err, "adding reaction failed")
+	// err = s.MessageReactionAdd(m.ChannelID, prompt.ID, "❌")
+	// return errors.Wrap(err, "adding reaction failed")
 
-	if 
-
-	_, err = s.ChannelMessageSendEmbed(heimdallr.Config.AdminLogChannel, &discordgo.MessageEmbed{
-		Title: fmt.Sprintf("%s Messages  were cleared. The command was made by %s", number, author.Mention()),
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:  "**Username**",
-				Value: author.User.Username + "#" + author.User.Discriminator,
-			},
-		},
-		Color: 0xEE0000,
-	})
-
+	// _, err = s.ChannelMessageSendEmbed(heimdallr.Config.AdminLogChannel, &discordgo.MessageEmbed{
+	// 	Title: fmt.Sprintf("%s Messages  were cleared. The command was made by %s", number, author.Mention()),
+	// 	Fields: []*discordgo.MessageEmbedField{
+	// 		{
+	// 			Name:  "**Username**",
+	// 			Value: author.User.Username + "#" + author.User.Discriminator,
+	// 		},
+	// 	},
+	// 	Color: 0xEE0000,
+	// })
+	return nil
 }
