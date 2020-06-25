d3de9592f1e558a20246b772359e37244df725c2
Malborne <ia1306439@qu.edu.qa>
2020-06-25 13:47:55 +0300

    detect links and files

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func main() {
 	dg.AddHandler(heimdallr.UserJoinHandler)
 	dg.AddHandler(heimdallr.UserLeaveHandler)
 	dg.AddHandler(heimdallr.MemberBanAddHandler)
+	dg.AddHandler(heimdallr.LinksAndFilesHandler)
 
 	err = dg.Open()
 	if err != nil {
