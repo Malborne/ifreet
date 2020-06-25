8ee0d2fc798348160ac7fbdb73d988ded88fcc2a
Malborne <ia1306439@qu.edu.qa>
2020-06-25 05:59:53 +0300

    testing

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	dg.AddHandler(heimdallr.UserJoinHandler)
 	dg.AddHandler(heimdallr.UserLeaveHandler)
 	dg.AddHandler(heimdallr.MemberBanAddHandler)
-	dg.AddHandler(commands.ReactionPrompt)
+	// dg.AddHandler(commands.ReactionPrompt)
 
 	err = dg.Open()
 	if err != nil {
