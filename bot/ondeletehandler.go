bee32b8b080d7f8a000fc618a86532919dd7619d
Malborne <ia1306439@qu.edu.qa>
2020-06-26 11:15:09 +0300

    archive

--- bot/ondeletehandler.go
+++ bot/ondeletehandler.go
@@ -12,7 +12,7 @@ func OnDeleteHandler(s *discordgo.Session, m *discordgo.MessageDelete) {
 	// if m.Author.Bot {
 	// 	return
 	// }
-	s.ChannelMessageSend(Config.ArchiveChannel, fmt.Sprintf("Message \"%s\" deletion detected with author %s.", m.Message.Content, m.Author.Username))
+	s.ChannelMessageSend(Config.ArchiveChannel, fmt.Sprintf("Message \"%s\" deletion detected with author %s.", m.Message.Content, m.Author.ID))
 
 	guildID := m.GuildID
 
@@ -44,6 +44,10 @@ func OnDeleteHandler(s *discordgo.Session, m *discordgo.MessageDelete) {
 		},
 		Color: 0xEE0000,
 	})
+	if err != nil {
+		LogIfError(s, errors.Wrap(err, "sending embed failed"))
+		return
+	}
 	_, err = s.ChannelMessageSendEmbed(Config.ArchiveChannel, &discordgo.MessageEmbed{
 		Title: fmt.Sprintf("A message was deleted"),
 		Fields: []*discordgo.MessageEmbedField{
