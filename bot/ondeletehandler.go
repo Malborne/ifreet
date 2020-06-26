1585680fe26b4683147ced31a5a034ab044f7c4b
Malborne <ia1306439@qu.edu.qa>
2020-06-26 10:34:58 +0300

    archive

--- bot/ondeletehandler.go
+++ bot/ondeletehandler.go
@@ -12,15 +12,16 @@ func OnDeleteHandler(s *discordgo.Session, m *discordgo.MessageDelete) {
 	// if m.Author.Bot {
 	// 	return
 	// }
+	s.ChannelMessageSend(Config.AdminLogChannel, fmt.Sprintf("Message delection detected."))
 	guildID := m.GuildID
 
 	author, err := GetMember(s, guildID, m.Author.ID)
 	if err != nil {
-		_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Message Author with ID %s was not found.", m.Author.ID))
+		_, err := s.ChannelMessageSend(Config.AdminLogChannel, fmt.Sprintf("Message Author with ID %s was not found.", m.Author.ID))
 		LogIfError(s, err)
 	}
 
-	_, err = s.ChannelMessageSendEmbed(Config.AdminLogChannel, &discordgo.MessageEmbed{
+	_, err = s.ChannelMessageSendEmbed(Config.ArchiveChannel, &discordgo.MessageEmbed{
 		Title: fmt.Sprintf("A message was deleted"),
 		Fields: []*discordgo.MessageEmbedField{
 			{
