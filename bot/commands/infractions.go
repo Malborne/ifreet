96f7a6c97b866e97c3fce3fb201782d3bf74ffd4
Malborne <ia1306439@qu.edu.qa>
2020-08-01 21:58:46 +0300

    infractions ID

--- bot/commands/infractions.go
+++ bot/commands/infractions.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
 	heimdallr "github.com/Malborne/ifreet/tree/master/bot"
@@ -53,7 +52,7 @@ func commandViewInfractions(s *discordgo.Session, m *discordgo.MessageCreate, ar
 	var fields []*discordgo.MessageEmbedField
 	for _, infraction := range infractions {
 		fields = append(fields, &discordgo.MessageEmbedField{
-			Name:  strconv.Itoa(infraction.ID) + "\n" + infraction.Time.Format(time.RFC1123),
+			Name:  (infraction.ID) + "\n" + infraction.Time.Format(time.RFC1123),
 			Value: infraction.Reason,
 		})
 	}
