3823510cec948c13c1a8394b19bf54aac3aa7d21
Malborne <ia1306439@qu.edu.qa>
2020-08-08 21:27:24 +0300

    Hifz circles commands

--- bot/database.go
+++ bot/database.go
@@ -96,11 +96,7 @@ CREATE TABLE IF NOT EXISTS mutedUsers (
 );
 
 
-CREATE TABLE IF NOT EXISTS students (
-	id TEXT PRIMARY KEY,
-	circle TEXT,
-	sheetLink TEXT,
-);
+
 
 CREATE TABLE IF NOT EXISTS archive (
 	id SERIAL PRIMARY KEY,
@@ -224,7 +220,7 @@ func RemoveInfraction(ID string) error {
 //AddStudent adds a new student to the database
 func AddStudent(userID string, circle string, sheetLink string) error {
 
-	_, err := db.Exec("INSERT INTO students (id, circle, sheetLink) VALUES ($1, $2, $3)",
+	_, err := db.Exec("INSERT INTO students (userID, circle, sheetLink) VALUES ($1, $2, $3)",
 		userID, circle, sheetLink)
 	return errors.Wrap(err, "adding student failed")
 }
@@ -240,7 +236,7 @@ func GetStudent(userID string) (Student, error) {
 		db.SetMaxOpenConns(db.Stats().MaxOpenConnections)
 	}
 	rows, err := db.Query(
-		"SELECT id, circle, sheetLink FROM students WHERE id=$1",
+		"SELECT userID, circle, sheetLink FROM students WHERE userID=$1",
 		userID,
 	)
 	if err != nil {
@@ -265,7 +261,7 @@ func GetStudent(userID string) (Student, error) {
 //RemoveStudent removes a student from the database
 func RemoveStudent(userID string) error {
 	_, err := db.Query(
-		"DELETE FROM students WHERE id = $1",
+		"DELETE FROM students WHERE userID = $1",
 		userID,
 	)
 	return errors.Wrap(err, "deleting student failed")
