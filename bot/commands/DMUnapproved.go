2eede62b85a8a5bff18267fc9d8091c17a15f001
Malborne <ia1306439@qu.edu.qa>
2021-05-02 23:24:33 +0300

    dm unapproved

--- bot/commands/DMUnapproved.go
+++ bot/commands/DMUnapproved.go
@@ -34,11 +34,11 @@ func commandDMUnapproved(s *discordgo.Session, m *discordgo.MessageCreate, args 
 	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("There are %d members in this guild", len(members)))
 
 	var count int = 0
+	var dmednum int = 0
 
 	for _, member := range members {
 
 		if !isApproved(member) && !member.User.Bot {
-			count = count + 1
 			userChannel, err := s.UserChannelCreate(member.User.ID)
 			if err != nil {
 				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s Does NOT ACCEPT DMs", member.Mention()))
@@ -57,16 +57,17 @@ func commandDMUnapproved(s *discordgo.Session, m *discordgo.MessageCreate, args 
 			}
 
 			if !AlreadyDMed {
-				_, err = s.ChannelMessageSend(userChannel.ID, fmt.Sprintf(
-					"You are an unapproved member of Quran Learning Center Server and you do not have access to most of the server. If you would like to have access to the server, please contact one of the moderators in the %s channel below to be approved.\n\n\nhttps://discord.gg/R6jKWT\n\nKeep in mind that if you stay for longer than a week without getting approved, you will risk being kicked out of the server.\n\nYou cannot reply to this message.", heimdallr.Config.WelcomeChannel))
-				if err != nil {
-					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s Does NOT ACCEPT DMs", member.Mention()))
-					heimdallr.LogIfError(s, errors.Wrap(err, "sending message failed. User Does NOT ACCEPT DMs"))
-				} else {
-					_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent a message to %s", member.Mention()))
+				dmednum = dmednum + 1
+				// _, err = s.ChannelMessageSend(userChannel.ID, fmt.Sprintf(
+				// 	"You are an unapproved member of Quran Learning Center Server and you do not have access to most of the server. If you would like to have access to the server, please contact one of the moderators in the %s channel below to be approved.\n\n\nhttps://discord.gg/R6jKWT\n\nKeep in mind that if you stay for longer than a week without getting approved, you will risk being kicked out of the server.\n\nYou cannot reply to this message.", heimdallr.Config.WelcomeChannel))
+				// if err != nil {
+				// 	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s Does NOT ACCEPT DMs", member.Mention()))
+				// 	heimdallr.LogIfError(s, errors.Wrap(err, "sending message failed. User Does NOT ACCEPT DMs"))
+				// } else {
+				// 	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent a message to %s", member.Mention()))
 
-					count = count + 1
-				}
+				// 	count = count + 1
+				// }
 			}
 		}
 	}
