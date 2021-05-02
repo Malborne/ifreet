32d87fda1a9a52a0238c87693825d05f48bb53c3
Malborne <ia1306439@qu.edu.qa>
2021-05-02 17:50:57 +0300

    dm unapproved

--- bot/commands/DMUnapproved.go
+++ bot/commands/DMUnapproved.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	heimdallr "github.com/Malborne/ifreet/tree/master/bot"
+	"github.com/pkg/errors"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/docopt/docopt-go"
@@ -28,35 +29,34 @@ func commandDMUnapproved(s *discordgo.Session, m *discordgo.MessageCreate, args 
 	if err != nil {
 		return err
 	}
-	// members, err := s.GuildMembers(m.GuildID, "", 1000)
-	// var count int = 0
+	var count int = 0
 
-	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("There are %d members in this guild", len(guild.Members)))
-	return err
-	// for _, member := range members {
-	// 	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s", member.Mention()))
+	// _, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("There are %d members in this guild", len(guild.Members)))
+	for _, member := range guild.Members {
 
-	// 	if !isApproved(member) && !member.User.Bot {
-	// 		count = count + 1
-	// 		userChannel, err := s.UserChannelCreate(member.User.ID)
-	// 		if err != nil {
-	// 			return errors.Wrap(err, "creating private channel failed")
-	// 		}
-	// 		_, err = s.ChannelMessageSend(userChannel.ID, fmt.Sprintf(
-	// 			"You are an unapproved member of Quran Learning Center Server and you do not have access to most of the server. If you would like to have access to the server, please contact one of the moderators in the %s channel below to be approved.\n\n\nhttps://discord.gg/R6jKWT\n\nKeep in mind that if you stay for longer than a week without getting approved, you will risk being kicked out of the server.\n\nYou cannot reply to this message.", heimdallr.Config.WelcomeChannel))
-	// 		if err != nil {
-	// 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s Does NOT ACCEPT DMs", member.User.Mention()))
-	// 			// return errors.Wrap(err, "sending message failed")
-	// 		}
-	// 	}
-	// }
+		if !isApproved(member) && !member.User.Bot {
 
-	// if count == 0 {
-	// 	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("No unapproved users found."))
-	// 	return errors.Wrap(err, "sending message failed")
-	// } else {
-	// 	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent messages to %d user(s)", count))
-	// 	return errors.Wrap(err, "sending message failed")
-	// }
+			userChannel, err := s.UserChannelCreate(member.User.ID)
+			if err != nil {
+				return errors.Wrap(err, "creating private channel failed")
+			}
+			_, err = s.ChannelMessageSend(userChannel.ID, fmt.Sprintf(
+				"You are an unapproved member of Quran Learning Center Server and you do not have access to most of the server. If you would like to have access to the server, please contact one of the moderators in the %s channel below to be approved.\n\n\nhttps://discord.gg/R6jKWT\n\nKeep in mind that if you stay for longer than a week without getting approved, you will risk being kicked out of the server.\n\nYou cannot reply to this message.", heimdallr.Config.WelcomeChannel))
+			if err != nil {
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s Does NOT ACCEPT DMs", member.Mention()))
+				// return errors.Wrap(err, "sending message failed")
+			} else {
+				count = count + 1
+			}
+		}
+	}
+
+	if count == 0 {
+		_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("No unapproved users found."))
+		return errors.Wrap(err, "sending message failed")
+	} else {
+		_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Sucessfully sent messages to %d user(s)", count))
+		return errors.Wrap(err, "sending message failed")
+	}
 
 }
