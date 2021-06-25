e2e9d74d07bd89752d6ffb033a70b7d2d6df6063
Malborne <ia1306439@qu.edu.qa>
2021-06-25 08:57:47 +0300

    clear fix

--- bot/newMemberHandler.go
+++ bot/newMemberHandler.go
@@ -27,8 +27,8 @@ func NewMemberJoinHandler(s *discordgo.Session, g *discordgo.GuildMemberAdd) {
 	// Type  PermissionOverwriteType `json:"type"`
 	// Deny  int64                   `json:"deny,string"`
 	// Allow int64                   `json:"allow,string"`
-	// deniedPermissions := []int64{0x0000000001, 0x0000000400, 0x0000000800, 0x0000001000, 0x0000004000, 0x0000008000, 0x0000010000, 0x0000020000, 0x0000040000, 0x0000080000, 0x0080000000, 0x0800000000, 0x1000000000}
-	deniedPermissions := []int64{0x0000000400, 0x0000000800}
+	deniedPermissions := []int64{0x0000000001, 0x0000000400, 0x0000000800, 0x0000001000, 0x0000004000, 0x0000008000, 0x0000010000, 0x0000020000, 0x0000040000, 0x0000080000, 0x0080000000, 0x0800000000, 0x1000000000}
+	// deniedPermissions := []int64{0x0000000400, 0x0000000800}
 
 	permissionObjects := DenyPermissions(s, Config.UserRole, deniedPermissions)
 	s.ChannelMessageSend(Config.AdminChannel, fmt.Sprintf("There are %d permission objtects", len(permissionObjects)))
@@ -39,7 +39,6 @@ func NewMemberJoinHandler(s *discordgo.Session, g *discordgo.GuildMemberAdd) {
 		LogIfError(s, errors.Wrap(err, "Creating New Channel failed"))
 
 	}
-
 	// deniedPermissions := []int{0x0000000400, 0x0000000800}
 
 	// allowedUserPermissions := []int{0x400, 0x800, 0x10000}
